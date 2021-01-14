2a650e8537a3e2614319b58418b7dd2ea1b59ba0
olliepayne <olliepayne@me.com>
2021-01-14 14:36:29 -0700

    polish basic nav styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav>
       <h2>MERN Books</h2>
       <ul className="nav-links">
-        <li><a href="/books">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/books">All Books</a></li>
         <li><a href="/books/new">Add Book</a></li>
       </ul>
