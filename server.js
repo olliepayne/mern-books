e2979cd696fc74071ad6b3b0ef1a875b18ee7411
olliepayne <olliepayne@me.com>
2021-01-14 14:00:33 -0700

    favicon and static

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ require('./config/database')
 
 app.use(logger('dev'))
 app.use(express.json())
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.listen(port, () => {
   console.log(`Server connected, listening on port ${port}...`)
