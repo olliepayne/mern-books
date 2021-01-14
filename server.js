4d9b4d58d79f13dd19124cba74c19a7c1c163eda
olliepayne <olliepayne@me.com>
2021-01-14 13:59:24 -0700

    mount basic middleware

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
 
 const app = express()
 const port = process.env.PORT || 3001
 
 require('dotenv').config()
 require('./config/database')
+
+app.use(logger('dev'))
+app.use(express.json())
 
 app.listen(port, () => {
   console.log(`Server connected, listening on port ${port}...`)
