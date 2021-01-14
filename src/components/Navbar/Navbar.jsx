7ea02b283ecb3335f71f5e0edfdf5f0a8397a41c
olliepayne <olliepayne@me.com>
2021-01-14 14:30:56 -0700

    stub navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <nav>
+      <h2>MERN Books</h2>
+      <ul>
+        <li><a href="/books">Home</a></li>
+        <li><a href="/books">All Books</a></li>
+        <li><a href="/books/new">Add Book</a></li>
+      </ul>
+    </nav>
+  )
+}
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react'
+import { Route } from 'react-router-dom'
 import './App.css';
+import * as booksAPI from '../../services/books-api'
+import Navbar from '../../components/Navbar/Navbar'
+import Landing from '../../pages/Landing/Landing'
 
 function App() {
+  useEffect(() => {
+
+  })
+
   return (
     <div className="App">
-      
+      <Navbar />
+      <Route
+        exact path="/"
+        render={() => {
+          <Landing />
+        }
+      }/>
     </div>
   );
 }
