6f0f71d9adb48fc2263640fce682139f275980b5
olliepayne <olliepayne@me.com>
2021-01-15 13:06:54 -0700

    styling tweaks

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -10,18 +10,36 @@ const Book = (props) => {
   }
 
   return (
-    <div className="book-card" onClick={handleOpen} >
-      <li key={index}>
+    // <div className="book-card" onClick={handleOpen} >
+    //   <li key={index}>
+    //     {isOpen ?
+    //       <>
+    //         <span className="book-title">{book.title}</span>, by {book.author}
+    //         {book.pages !== undefined ? <p>{book.pages} pages</p> : ''}
+    //         <button className="update-btn">Update</button>
+    //         <button className="delete-btn">Delete</button>
+    //       </>
+    //       :
+    //       <img className="book-image" src={book.imageURL} alt={book.title} />
+    //     }
+    //   </li>
+    // </div>
+    <li key={index}>
+      <div className="book-card" onClick={handleOpen} >
         {isOpen ?
-          <>
-            <span className="book-title">{book.title}</span>, by {book.author}
-            {book.pages !== undefined ? <p>{book.pages} pages</p> : ''}
-          </>
-          :
-          <img className="book-image" src={book.imageURL} alt={book.title} />
+            <>
+              <span className="book-title">{book.title}</span>, by {book.author}
+              {book.pages !== undefined ? <>{book.pages} pages</> : ''}
+              <div className="btn-container">
+                <button className="update-btn">Update</button>
+                <button className="delete-btn">Delete</button>
+              </div>
+            </>
+            :
+            <img className="book-image" src={book.imageURL} alt={book.title} />
         }
-      </li>
-    </div>
+      </div>
+    </li>
   )
 }
 
