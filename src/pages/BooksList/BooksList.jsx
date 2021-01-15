fbe8c5c6b63550763cd684c8ff7b5504bc393904
olliepayne <olliepayne@me.com>
2021-01-14 22:09:07 -0700

    add loading ternary

--- src/pages/BooksList/BooksList.jsx
+++ src/pages/BooksList/BooksList.jsx
@@ -1,14 +1,20 @@
 const BooksList = (props) => {
+  const { books } = props
+
   return (
     <div className="books-list-container">
       <h1>All Books.</h1>
-      <ul>
-        {props.books.map((book, index) => (
-          <li key={index}>{book.title}, by {book.author}
-            {book.pages !== null ? <p>{book.pages} pages</p> : ''}
-          </li>
-        ))}
-      </ul>
+      {books.length > 0 ?
+        <ul>
+          {props.books.map((book, index) => (
+            <li key={index}>{book.title}, by {book.author}
+              {book.pages !== null ? <p>{book.pages} pages</p> : ''}
+            </li>
+          ))}
+        </ul>
+        :
+        <p>Loading books...</p>
+      }
     </div>
   )
 }
